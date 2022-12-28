ada2eb393f01ec9d68f720756c09c105b2fba702
Daydre4m <ray040406@gmail.com>
2022-12-28 19:23:00 +1300

    fixing-repeating-setUpHand

--- oldscript.js
+++ oldscript.js
@@ -2,11 +2,26 @@ const deck = [];
 const p1Hand = [];
 const p2Hand = [];
 const setUpDeck = function () {
-  for (let i = 0; i <= 51; i++) {
+  for (let i = 0; i <= 52; i++) {
     deck.push(i);
   }
 };
 
+// const setUpHand = function (emptyHand) {
+//   for (let i = 0; i <= 4; i++) {
+//     console.log(deck);
+
+//     let cardIndex = Math.trunc(Math.random() * deck.length);
+//     console.log("index", cardIndex);
+
+//     let card = deck[cardIndex];
+//     console.log("card", card);
+//     emptyHand.push(card);
+
+//     deck.splice(cardIndex, 1);
+//     console.log(deck);
+//   }
+// };
 const setUpHand = function (hand) {
   // Randomize 5 cards from 52 and put into slots for each player
   // Input: none
@@ -16,13 +31,11 @@ const setUpHand = function (hand) {
     let index = Math.trunc(Math.random() * deck.length);
     console.log(index);
     hand.push(deck[index]);
-    deck.pop(index);
+    deck.splice(index, 1);
   }
 };
 setUpDeck();
-console.log(deck);
 setUpHand(p1Hand);
 setUpHand(p2Hand);
-
 console.log(p1Hand);
 console.log(p2Hand);
