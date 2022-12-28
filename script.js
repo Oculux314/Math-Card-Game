3ef333c70ece7d367fff20c98f943792bcd694f9
Nate Williamson <nwil508@UoA.auckland.ac.nz>
2022-12-28 23:20:02 +1300

    Tidying up script.js code from 28/12/2022. Now everything is in functions!

--- script.js
+++ script.js
@@ -1,126 +1,144 @@
-/* Helper Functions --------------------------------------------------------------------------------------------------------*/
-
-// For miscellaneous functions that are literally just there to help make another function easier to write
 
 /* Card Manipulator Functions ----------------------------------------------------------------------------------------------*/
-const deck = [];
-const p1Hand = [];
-const p2Hand = [];
 
 function addCardToDeck(card) {
-  // Parameters: (card) card value to add to deck [0 - 51]
-  // Actions: add the specifed card to the end of the deck
-  // Return: the number of cards now in the deck
+    // Parameters: (card) card value to add to deck [0 - 51]
+    // Actions: add the specifed card to the end of the deck
+    // Return: the number of cards now in the deck
 }
 
 function removeCardFromDeck(card) {
-  // Parameters: (card) card value to remove from deck [0 - 51]
-  // Actions: find the index of the specified card in the deck, remove it
-  // Return: the number of cards left in the deck, or -1 if the specified card was not in the deck
+    // Parameters: (card) card value to remove from deck [0 - 51]
+    // Actions: find the index of the specified card in the deck, remove it
+    // Return: the number of cards left in the deck, or -1 if the specified card was not in the deck
 }
 
 function setPlayerCard(player, position, card) {
-  // Parameters: (player) the player to set the card for [0 - 1], (position) index of hand [0 - 4], (card) card value [0 - 51]
-  // Actions: set the specified array element to the specified card value
-  // Return: the original card value at that position
+    // Parameters: (player) the player to set the card for [0 - 1], (position) index of hand [0 - 4], (card) card value [0 - 51]
+    // Actions: set the specified array element to the specified card value
+    // Return: the original card value at that position
 }
 
 function swapPlayerCards(player1, position1, player2, position2) {
-  // Parameters: (player1,position1) position of card 1, (player2,position2) position of card 2
-  // Actions: swap the card values at these two positions
-  // Return: none
+    // Parameters: (player1,position1) position of card 1, (player2,position2) position of card 2
+    // Actions: swap the card values at these two positions
+    // Return: none
 }
 
 /* Logic Functions ---------------------------------------------------------------------------------------------------------*/
 
 function evaluateHand(hand) {
-  // Parameters: (hand) a 5-length 'hand' array
-  // Actions: calculate the value of the hand
-  // Return: the hand value
+    // Parameters: (hand) a 5-length 'hand' array
+    // Actions: calculate the value of the hand
+    // Return: the hand value
 }
 
 /* Setup Functions ---------------------------------------------------------------------------------------------------------*/
 
-function setUpDeck() {
-  // Parameters: none
-  // Actions: create a 'deck' array of length 52 filled with one of each card value [0 - 51]
-  // Return: the 'deck' array
-  for (let i = 0; i <= 54; i++) {
-    deck.push(i);
-  }
+function restart() {
+    // Parameters: none
+    // Actions: calls all the other setup functions when restarting a game
+    // Return: none
+
+    const deck = setUpDeck();
+    const p1Hand = setUpHand(deck);
+    const p2Hand = setUpHand(deck);
+    const goalNum = chooseGoalNumber();
+
+    takeTurn();
 }
 
-function setUpHand(hand) {
-  // Parameters: (deck) a 52-length fully populated 'deck' array
-  // Actions: create a 'hand' array of length 5, randomly fill it with cards from the deck, remove those cards from the deck
-  // Return: the 'hand' array
-  for (i = 1; i <= 5; i++) {
-    let index = Math.trunc(Math.random() * deck.length);
-    console.log(index);
-    hand.push(deck[index]);
-    deck.splice(index, 1);
-  }
+function setUpDeck() {
+    // Parameters: none
+    // Actions: create a 'deck' array of length 52 filled with one of each card value [0 - 51]
+    // Return: the 'deck' array
+
+    const deck = [];
+
+    for (let i = 0; i < 52; i++) { // [0 - 51]
+        deck.push(i);
+    }
+
+    return deck;
+}
+
+function setUpHand(deck) {
+    // Parameters: (deck) a 52-length fully populated 'deck' array
+    // Actions: create a 'hand' array of length 5, randomly fill it with cards from the deck, remove those cards from the deck
+    // Return: the 'hand' array
+
+    const hand = [];
+
+    for (i = 1; i <= 5; i++) {
+        let index = Math.trunc(Math.random() * deck.length); // choose random card from deck
+        hand.push(deck[index]); // add to hand
+        deck.splice(index, 1); // remove from deck
+    }
+
+    return hand;
 }
 
 function chooseGoalNumber() {
-  // Parameters: none
-  // Actions: randomly choose a suitable 'score' number for both players to aim for
-  // Return: the 'score' number
-  const goalNum = Math.trunc(Math.random() * 1000);
+    // Parameters: none
+    // Actions: randomly choose a suitable 'score' number for both players to aim for
+    // Return: the 'score' number
 
-  console.log(goalNum);
+    const goalNum = Math.trunc(Math.random() * 1000); // [0 - 999]
+    return goalNum;
 }
 
 /* Runtime Functions -------------------------------------------------------------------------------------------------------*/
 
 function takeTurn() {
-  // Parameters: none
-  // Actions: idk lol
-  // Return: none
+    // Parameters: none
+    // Actions: idk lol
+    // Return: none
+
+    console.log("Success!");
 }
 
 /* Render Functions --------------------------------------------------------------------------------------------------------*/
 
 function render() {
-  // Parameters: tbc
-  // Actions: update all the html and css elements
-  // Return: none
+    // Parameters: tbc
+    // Actions: update all the html and css elements
+    // Return: none
 }
 
 function renderCard(id, cardValue) {
-  // Parameters: (id) id of card element, (cardValue) card value [0 - 51] of card in that position
-  // Actions: update a single card html element
-  // Return: none
+    // Parameters: (id) id of card element, (cardValue) card value [0 - 51] of card in that position
+    // Actions: update a single card html element
+    // Return: none
 }
 
 function renderHandScore(id, score) {
-  // Parameters: (id) id of score element, (score) score of current hand
-  // Actions: update a single score html element
-  // Return: none
+    // Parameters: (id) id of score element, (score) score of current hand
+    // Actions: update a single score html element
+    // Return: none
 }
 
 function renderGoalScore(score) {
-  // Parameters: (score) score both players are aiming for to win the game
-  // Actions: update the goal score html element
-  // Return: none
+    // Parameters: (score) score both players are aiming for to win the game
+    // Actions: update the goal score html element
+    // Return: none
 }
 
 function highlightElement(id) {
-  // Parameters: (id) id of element to highlight
-  // Actions: 'highlight' (enlarge slightly) the specified element
-  // Return: none
+    // Parameters: (id) id of element to highlight
+    // Actions: 'highlight' (enlarge slightly) the specified element
+    // Return: none
 }
 
 function unhighlightElement(id) {
-  // Parameters: (id) id of element to stop highlighting
-  // Actions: 'dehighlight' (return to original size) the specified element
-  // Return: none
+    // Parameters: (id) id of element to stop highlighting
+    // Actions: 'dehighlight' (return to original size) the specified element
+    // Return: none
 }
 
 function changeDeck(card) {
-  // Parameters: (card) card value to reveal from deck [0 - 51, or -1 for deck]
-  // Actions: switch deck card graphic to specified value
-  // Return: none
+    // Parameters: (card) card value to reveal from deck [0 - 51, or -1 for deck]
+    // Actions: switch deck card graphic to specified value
+    // Return: none
 }
 
 /* UI Functions ------------------------------------------------------------------------------------------------------------*/
@@ -128,22 +146,18 @@ function changeDeck(card) {
 // How do click event handlers work in JS??
 
 function onDeckClick() {
-  // Parameters: none
-  // Actions: handler for when deck clicked
-  // Return: none
+    // Parameters: none
+    // Actions: handler for when deck clicked
+    // Return: none
 }
 
 function onCardClick() {
-  // Parameters: none
-  // Actions: handler for when card clicked
-  // Return: id of card clicked
+    // Parameters: none
+    // Actions: handler for when card clicked
+    // Return: id of card clicked
 }
 
-// Functions calls
-setUpDeck();
-setUpHand(p1Hand);
-setUpHand(p2Hand);
-console.log(p1Hand);
-console.log(p2Hand);
 
-chooseGoalNumber();
+/* Global Calls ------------------------------------------------------------------------------------------------------------*/
+
+restart();
